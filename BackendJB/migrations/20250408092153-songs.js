9b14884a74ae6a7580696a1a42592abfaea9e7ff
Robbin Schrijver <robbinschrijver07@gmail.com>
2025-04-08 11:56:55 +0200

    added mysql2 package and working migrations using sequelize

---
+++ BackendJB/migrations/20250408092153-songs.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("songs", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      song_title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      song_duration: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("songs");
+  },
+};
---
+++ BackendJB/migrations/20250408092209-playlist.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("playlists", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        // NOTE relation to user tabel
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE", // wanneer de gebruiker wordt bijgewerkt, wordt de waarde van 'userId' in playlists bijgewerkt
+        onDelete: "CASCADE", // Wanneer de gebruiker wordt verwijderd, worden de bijbehorende playlists verwijderd
+      },
+      songId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "songs",
+          key: "id",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("playlists");
+  },
+};
