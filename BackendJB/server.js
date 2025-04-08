8781b84566e0731a658105151cec3677d8fa700f
Robbin Schrijver <robbinschrijver07@gmail.com>
2025-04-08 10:24:51 +0200

    another sequalize downlaod

--- BackendJB/server.js
+++ BackendJB/server.js
@@ -1,16 +1,10 @@
+// NOTE added sequelize for migrations
+
 const express = require("express");
-const mysql = require("mysql");
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
-
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "jukebox",
-});
 
 app.get("/", (re, res) => {
   return res.json("From backend side");
