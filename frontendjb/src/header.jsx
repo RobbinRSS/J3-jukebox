f309ced83aa09781acd28196a41c2fe8463c9e65
Robbin Schrijver <robbinschrijver07@gmail.com>
2025-04-10 11:53:25 +0200

    added logic for sign in and sign up, still need to make logic if user makes a new account so it will be added to mysql database

--- frontendjb/src/header.jsx
+++ frontendjb/src/header.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
-import "./App.css";
+import "./index.css";
 
-export function PopupContent({ setUsernameFromLogin }) {
+export function PopupContent({ setUsernameFromLogin, loginType }) {
   const [data, setData] = useState([]);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:8081/users")
@@ -16,15 +17,21 @@ export function PopupContent({ setUsernameFromLogin }) {
   const handleSubmit = function (e) {
     e.preventDefault();
 
+    const userNameFound = data.find((user) => user.name === username);
+
     const userFound = data.find(
       (user) => user.name === username && user.password === password
     );
 
-    if (userFound) {
-      console.log("Logged in successfully!");
+    if (userNameFound && loginType === "signUp") {
+      setErrorMessage("Username already exists");
+    } else if (userFound && loginType === "signIn") {
       setUsernameFromLogin(username);
-    } else {
-      console.log("nuh uh");
+    } else if (!userFound && loginType === "signIn") {
+      setErrorMessage("User is not found");
+    } else if (!userNameFound && loginType === "signUp") {
+      console.log("Account succesfully created");
+      // TODO logica om aan de sql database toe te voegen
     }
   };
 
@@ -51,6 +58,7 @@ export function PopupContent({ setUsernameFromLogin }) {
           <br />
           <button type="submit">Submit</button>
         </form>
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
       </div>
     </>
   );
@@ -59,8 +67,20 @@ export function PopupContent({ setUsernameFromLogin }) {
 function Header() {
   const [showModal, setShowModal] = useState(false);
   const [username, setUsername] = useState("");
+  const [type, setLoginType] = useState("");
+
   const openModal = () => setShowModal(true);
   const closeModal = () => setShowModal(false);
+
+  const openSignIn = () => {
+    setLoginType("signIn");
+    openModal();
+  };
+
+  const openSignUp = () => {
+    setLoginType("signUp");
+    openModal();
+  };
 
   const setUsernameFromLogin = (name) => {
     setUsername(name);
@@ -73,8 +93,8 @@ function Header() {
         {(!username && (
           <>
             <div id="header-btns">
-              <button onClick={openModal}>Sign Up</button>
-              <button onClick={openModal}>Sign In</button>
+              <button onClick={openSignUp}>Sign Up</button>
+              <button onClick={openSignIn}>Sign In</button>
             </div>
           </>
         )) || (
@@ -89,7 +109,10 @@ function Header() {
       {showModal && (
         <>
           <div className="overlay" onClick={closeModal}></div>
-          <PopupContent setUsernameFromLogin={setUsernameFromLogin} />
+          <PopupContent
+            setUsernameFromLogin={setUsernameFromLogin}
+            loginType={type}
+          />
         </>
       )}
     </>
